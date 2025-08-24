d85617ec9e754d50d0f159beb49c091c7c47e21e
michaelfuchaka <fuchakamichael06@gmail.com>
2025-08-25 00:21:57 +0300

    Implement Stack class methods: push, pop, peek, size, full, and search

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,41 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items = None, limit = 100):
+        self.items = list(items) if items  else []
+        self.limit = limit
+
+      
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
+       
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+         self.items.append(item)
+        else:
+            return None
+        
 
     def pop(self):
-        pass
-
+        if self.isEmpty():
+            return None 
+        return self.items.pop()
+        
     def peek(self):
-        pass
-    
+        if self.isEmpty():
+            return None
+        return self.items[-1]
+      
     def size(self):
-        pass
+        return len(self.items)
+        
 
     def full(self):
-        pass
+          return len(self.items) >= self.limit
+ 
 
     def search(self, target):
-        pass
+         if target in self.items:
+            return len(self.items) - 1 - self.items.index(target)
+         return -1
